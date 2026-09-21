cd1957e93b662a0a36c92cef617e2fd682a2fcf7
LowDmoonx <Eduoliveira45168@gmail.com>
2026-09-21 20:01:17 -0300

    Modelos iniciais

---
+++ main.js
@@ -0,0 +1,28 @@
+/* MODELO 1
+var a = 5
+var b = 7
+var c = a+b
+console.log(c)
+FIM MODELO 1*/
+
+/* MODELO 2
+ var a = 3
+    var b = 4
+function somar (x,  y){
+   var c = x+y
+   return c
+}
+
+console.log(somar (a,b))
+FIM MODELO 2*/
+
+/* MODELO 3
+var a = 1
+var b = 2
+const d = 0
+
+function somar (x,y){
+let c = x+y
+return c
+}
+FIM MODELO 3*/
